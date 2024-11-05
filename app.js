87d76f3806799d57cba68f8247416ddd9550eb61
afifahrahmak <afifah.ark@gmail.com>
2024-11-05 13:38:27 +0700

    register done

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/register', (req, res) => {
+  res.send('POST request to the homepage')
+  // code register in here
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
